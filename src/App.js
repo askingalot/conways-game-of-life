10208f7bb1c1169cddd6b83ac4ab68cf1f7c8f39
Andy Collins <askingalot@gmail.com>
2020-04-18 09:43:55 -0500

    alive and dead cells

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 import Cell from './Cell';
 
 function App() {
-  const board = [...Array(40).keys()].map((_, i) => <Cell key={i} isAlive={i % 2 === 0}/>);
+  const boardSize = 20;
+
+
+  const board = [...Array(boardSize).keys()].map(i => <Cell key={i} isAlive={i % 2 !== 0}/>);
+
   return (
     <div id="app">
       {board}
--- src/Cell.js
+++ src/Cell.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default function Cell({ isAlive }) {
-  const classes = { "cell": true, "is-alive": isAlive };
+  const classes = 'cell ' + (isAlive ? 'is-alive' : '');
   return <div className={classes}>Foo</div>;
 }
